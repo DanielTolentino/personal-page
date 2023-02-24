674b0c0ead57442e28c6f6df74045517db9cea19
DanielTolentino <danieltolentino@protonmail.com>
2023-02-23 22:34:06 -0300

    Initial index and populate to test NavBar scroll

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 import Head from 'next/head';
+import Link from 'next/link'
 import styles from '../styles/Home.module.css';
+
+import NavBar from '../components/NavBar';
 
 export default function Home() {
   return (
@@ -8,45 +11,57 @@ export default function Home() {
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+      
       <main>
+      <div>
+        <NavBar/>
+      </div>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
+      <div>
+      </div>
+        <h1 className={styles.title}>
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        </h1>
+      <div>
+      </div>
+        <h1 className={styles.title}>
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        </h1>
+      <div>
+      </div>
+        <h1 className={styles.title}>
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        </h1>
+      <div>
+      </div>
+        <h1 className={styles.title}>
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        </h1>
+      <div>
+      </div>
+        <h1 className={styles.title}>
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        </h1>
+      <div>
+      </div>
+        <h1 className={styles.title}>
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        </h1>
+      <div>
+        <p>
+          {/* make a lorem ipsum */}
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae
+          tincidunt nisl. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla
+          facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla
+          facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla
+          facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla
+          facilisi. Nulla facilisi. Nulla facilisi. Nulla facilisi. Nulla
 
-        <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
         </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+      </div>
+       
       </main>
 
       <footer>
