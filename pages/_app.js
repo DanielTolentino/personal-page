6e1b7033b180fad37e38c90afdcc946638feb047
DanielTolentino <danieltolentino@protonmail.com>
2023-02-25 00:01:53 -0300

    Setup i18n

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,23 @@
 import { Analytics } from '@vercel/analytics/react';
+import { useRouter } from "next/router";
+import {NextIntlProvider} from 'next-intl';
 import '../styles.css'
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Component {...pageProps} />
+      <NextIntlProvider messages={pageProps.messages}>
+        <Component {...pageProps} />
+      </NextIntlProvider>
       <Analytics />
     </>
   );
 }
-
+export async function getStaticProps({locale}) {
+  return {
+    props: {
+      messages: (await import(`../lang/${locale}.json`)).default
+    }
+  };
+}
 export default MyApp;
