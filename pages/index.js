64bc82774163bc3fca9a0f7e63ac6c37b14bc211
DanielTolentino <danieltolentino@protonmail.com>
2023-02-27 17:17:28 -0300

    Format document

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
-import Head from 'next/head';
-import Link from 'next/link'
-import Image from 'next/image';
-import styles from '../styles/Home.module.css';
+import Head from "next/head";
+import Link from "next/link";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
 
-import Card from '../components/ProjectCard';
-import NavBar from '../components/NavBar';
+import Card from "../components/ProjectCard";
+import NavBar from "../components/NavBar";
 
 export default function Home() {
   return (
@@ -13,44 +13,110 @@ export default function Home() {
         <title>Início</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      
-      <main>
-      <img src="/foto_de_perfil.webp" alt="profile pic" className={styles.pfp} />
-      <div>
-        <NavBar/>
-        <div>          
-          <div>
-            <h1 className={styles.title}>Olá <span className="emoji"> 👋 </span>, meu nome é Daniel Tolentino </h1>
-            <br />
-            <div className={styles.links_row}>
-              
-                <a  className={styles.link_card}                href="https://github.com/DanielTolentino"
-                target="_blank"
-                rel="noopener noreferrer"
-                >
-                 <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g clipPath="url(#clip0_9914_10)"><path d="M12 0C5.374 0 0 5.373 0 12C0 17.302 3.438 21.8 8.207 23.387C8.806 23.498 9 23.126 9 22.81V20.576C5.662 21.302 4.967 19.16 4.967 19.16C4.421 17.773 3.634 17.404 3.634 17.404C2.545 16.659 3.717 16.675 3.717 16.675C4.922 16.759 5.556 17.912 5.556 17.912C6.626 19.746 8.363 19.216 9.048 18.909C9.155 18.134 9.466 17.604 9.81 17.305C7.145 17 4.343 15.971 4.343 11.374C4.343 10.063 4.812 8.993 5.579 8.153C5.455 7.85 5.044 6.629 5.696 4.977C5.696 4.977 6.704 4.655 8.997 6.207C9.954 5.941 10.98 5.808 12 5.803C13.02 5.808 14.047 5.941 15.006 6.207C17.297 4.655 18.303 4.977 18.303 4.977C18.956 6.63 18.545 7.851 18.421 8.153C19.191 8.993 19.656 10.064 19.656 11.374C19.656 15.983 16.849 16.998 14.177 17.295C14.607 17.667 15 18.397 15 19.517V22.81C15 23.129 15.192 23.504 15.801 23.386C20.566 21.797 24 17.3 24 12C24 5.373 18.627 0 12 0Z" fill="currentColor"></path></g><defs><clipPath id="clip0_9914_10"><rect width="24" height="24" fill="white"></rect></clipPath></defs></svg> GitHub 
-                 <span>
-                  <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M2.07102 11.3494L0.963068 10.2415L9.2017 1.98864H2.83807L2.85227 0.454545H11.8438V9.46023H10.2955L10.3097 3.09659L2.07102 11.3494Z" fill="currentColor"></path></svg></span>
-                </a>
-              
 
-              
-              <a className={styles.link_card} href="https://www.linkedin.com/in/daniel-tolentino-ferreira-santos/"
-                target="_blank"
-                rel="noopener noreferrer"
+      <main>
+        <img
+          src="/foto_de_perfil.webp"
+          alt="profile pic"
+          className={styles.pfp}
+        />
+        <div>
+          <NavBar />
+          <div>
+            <div>
+              <h1 className={styles.title}>
+                Olá <span className="emoji"> 👋 </span>, meu nome é Daniel
+                Tolentino{" "}
+              </h1>
+              <br />
+              <div className={styles.links_row}>
+                <a
+                  className={styles.link_card}
+                  href="https://github.com/DanielTolentino"
+                  target="_blank"
+                  rel="noopener noreferrer"
                 >
-                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30" width="20px" height="20px"><path d="M15,3C8.373,3,3,8.373,3,15c0,6.627,5.373,12,12,12s12-5.373,12-12C27,8.373,21.627,3,15,3z M10.496,8.403 c0.842,0,1.403,0.561,1.403,1.309c0,0.748-0.561,1.309-1.496,1.309C9.561,11.022,9,10.46,9,9.712C9,8.964,9.561,8.403,10.496,8.403z M12,20H9v-8h3V20z M22,20h-2.824v-4.372c0-1.209-0.753-1.488-1.035-1.488s-1.224,0.186-1.224,1.488c0,0.186,0,4.372,0,4.372H14v-8 h2.918v1.116C17.294,12.465,18.047,12,19.459,12C20.871,12,22,13.116,22,15.628V20z"
-                 fill="currentColor"></path><defs><clipPath id="clip0_9914_10"><rect width="24" height="24" fill="white"></rect></clipPath></defs></svg>
-                 Linkedin 
-                 <span>
-                  <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M2.07102 11.3494L0.963068 10.2415L9.2017 1.98864H2.83807L2.85227 0.454545H11.8438V9.46023H10.2955L10.3097 3.09659L2.07102 11.3494Z" fill="currentColor"></path></svg></span>
+                  <svg
+                    width="20"
+                    height="20"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <g clipPath="url(#clip0_9914_10)">
+                      <path
+                        d="M12 0C5.374 0 0 5.373 0 12C0 17.302 3.438 21.8 8.207 23.387C8.806 23.498 9 23.126 9 22.81V20.576C5.662 21.302 4.967 19.16 4.967 19.16C4.421 17.773 3.634 17.404 3.634 17.404C2.545 16.659 3.717 16.675 3.717 16.675C4.922 16.759 5.556 17.912 5.556 17.912C6.626 19.746 8.363 19.216 9.048 18.909C9.155 18.134 9.466 17.604 9.81 17.305C7.145 17 4.343 15.971 4.343 11.374C4.343 10.063 4.812 8.993 5.579 8.153C5.455 7.85 5.044 6.629 5.696 4.977C5.696 4.977 6.704 4.655 8.997 6.207C9.954 5.941 10.98 5.808 12 5.803C13.02 5.808 14.047 5.941 15.006 6.207C17.297 4.655 18.303 4.977 18.303 4.977C18.956 6.63 18.545 7.851 18.421 8.153C19.191 8.993 19.656 10.064 19.656 11.374C19.656 15.983 16.849 16.998 14.177 17.295C14.607 17.667 15 18.397 15 19.517V22.81C15 23.129 15.192 23.504 15.801 23.386C20.566 21.797 24 17.3 24 12C24 5.373 18.627 0 12 0Z"
+                        fill="currentColor"
+                      ></path>
+                    </g>
+                    <defs>
+                      <clipPath id="clip0_9914_10">
+                        <rect width="24" height="24" fill="white"></rect>
+                      </clipPath>
+                    </defs>
+                  </svg>{" "}
+                  GitHub
+                  <span>
+                    <svg
+                      width="12"
+                      height="12"
+                      viewBox="0 0 12 12"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M2.07102 11.3494L0.963068 10.2415L9.2017 1.98864H2.83807L2.85227 0.454545H11.8438V9.46023H10.2955L10.3097 3.09659L2.07102 11.3494Z"
+                        fill="currentColor"
+                      ></path>
+                    </svg>
+                  </span>
                 </a>
-                
+
+                <a
+                  className={styles.link_card}
+                  href="https://www.linkedin.com/in/daniel-tolentino-ferreira-santos/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 30 30"
+                    width="20px"
+                    height="20px"
+                  >
+                    <path
+                      d="M15,3C8.373,3,3,8.373,3,15c0,6.627,5.373,12,12,12s12-5.373,12-12C27,8.373,21.627,3,15,3z M10.496,8.403 c0.842,0,1.403,0.561,1.403,1.309c0,0.748-0.561,1.309-1.496,1.309C9.561,11.022,9,10.46,9,9.712C9,8.964,9.561,8.403,10.496,8.403z M12,20H9v-8h3V20z M22,20h-2.824v-4.372c0-1.209-0.753-1.488-1.035-1.488s-1.224,0.186-1.224,1.488c0,0.186,0,4.372,0,4.372H14v-8 h2.918v1.116C17.294,12.465,18.047,12,19.459,12C20.871,12,22,13.116,22,15.628V20z"
+                      fill="currentColor"
+                    ></path>
+                    <defs>
+                      <clipPath id="clip0_9914_10">
+                        <rect width="24" height="24" fill="white"></rect>
+                      </clipPath>
+                    </defs>
+                  </svg>
+                  Linkedin
+                  <span>
+                    <svg
+                      width="12"
+                      height="12"
+                      viewBox="0 0 12 12"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M2.07102 11.3494L0.963068 10.2415L9.2017 1.98864H2.83807L2.85227 0.454545H11.8438V9.46023H10.2955L10.3097 3.09659L2.07102 11.3494Z"
+                        fill="currentColor"
+                      ></path>
+                    </svg>
+                  </span>
+                </a>
+              </div>
+              <h2 className={styles.description}>
+                Gosto de tecnologia, Linux e o mundo Open Source!
+              </h2>
             </div>
-            <h2 className={styles.description}>Gosto de tecnologia, Linux e o mundo Open Source!</h2>
           </div>
         </div>
-      </div>
       </main>
 
       <style jsx>{`
@@ -62,7 +128,7 @@ export default function Home() {
           justify-content: center;
           align-items: center;
         }
-        a{
+        a {
           display: flex;
           flex-direction: row;
           justify-content: center;
@@ -112,5 +178,5 @@ export default function Home() {
         }
       `}</style>
     </div>
-  )
+  );
 }
