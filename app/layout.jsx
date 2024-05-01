fe0405a3b1085422cc673f5bdcdf9800dfae5c27
Daniel Tolentino <danieltolentino@protonmail.com>
2024-05-01 15:54:38 -0300

    Add @vercel/speed-insights package and component (#14)
    * Add @vercel/speed-insights package and component
    
    * Refactor layout components in RootLayout to include Analytics and SpeedInsights components

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,5 @@
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "../app/styles/styles.css";
 import "../app/styles/globals.css";
 import "../app/styles/Home.module.css";
@@ -18,9 +19,10 @@ export default function RootLayout({
     <>
       <html>
         <body>
-          <Analytics />
           <NavBar />
           {children}
+          <Analytics />
+          <SpeedInsights />
         </body>
       </html>
     </>
